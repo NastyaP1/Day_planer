bf9a43e74958612882d1dc0640c6919e0a0e731a
daria-teremkova <dariaflorett09@gmail.com>
2020-03-28 01:18:22 +0300

    Added registration page

---
+++ organiser/organiser-app/src/app/registrationPage/app.registrationComponent.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: 'registrationPage',
+    templateUrl: './registrationPage.html',
+    styleUrls: ['./app.registrationComponent.css']
+})
+export class RegistrationPageComponent implements OnInit{
+    constructor(){}
+    ngOnInit(){}
+}
