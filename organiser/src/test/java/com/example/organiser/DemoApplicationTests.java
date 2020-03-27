ac945017e2ea71c2f61d0f6743aa72ec55d0d95d
NastyaP1 <anastasiapereslavtseva@gmail.com>
2020-03-27 23:42:16 +0300

    started project

---
+++ organiser/src/test/java/com/example/organiser/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.organiser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
