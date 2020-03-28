199dbfddcf2e20c4708ef5f0df5fae1819ef985c
daria-teremkova <dariaflorett09@gmail.com>
2020-03-28 22:53:22 +0300

    Added new log in page

---
+++ organiser/organiser-app/src/app/loginPage/app.loginComponent.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: 'loginPage',
+    templateUrl: './loginPage.html',
+    styleUrls: ['./app.loginComponent.css']
+})
+export class LoginPageComponent implements OnInit{
+    constructor(){}
+    ngOnInit(){}
+}
