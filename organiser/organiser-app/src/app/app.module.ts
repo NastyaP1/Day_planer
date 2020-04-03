25a82cd22e3d942cc3295e4cc25918b5d7aba7ab
daria-teremkova <dariaflorett09@gmail.com>
2020-04-03 13:46:30 +0300

    Added footer

--- organiser/organiser-app/src/app/app.module.ts
+++ organiser/organiser-app/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { LoginPageComponent } from './loginPage/app.loginComponent';
 import {RegistrationPageComponent} from './registrationPage/app.registrationComponent';
 import {SideBarComponent} from './sideBar/app.sideBarComponent';
+import {FooterComponent} from './footer/app.footerComponent';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginPageComponent,
     RegistrationPageComponent,
-    SideBarComponent
+    SideBarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ organiser/organiser-app/src/app/footer/app.footerComponent.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: 'own-footer',
+    templateUrl: './footer.html',
+    styleUrls: ['./app.footerComponent.css']
+})
+export class FooterComponent implements OnInit{
+    constructor(){}
+    ngOnInit(){}
+}
