0f88817053284e723e899ccc709cd816978d92d1
daria-teremkova <dariaflorett09@gmail.com>
2020-04-11 23:15:50 +0300

    Added domens and services in front

---
+++ organiser/my-app/src/app/services/event.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Event} from "../domens/event";
+
+{
+providedIn: 'root'
+}
+export class EventService {
+private eventUrl : string;
+
+constructor(private http: HttpClient) {
+this.eventUrl = "http://localhost:8182/api/user/3/events/"; 
+}
+getById(id) : Observable<Event>{
+return this.http.get<Event>(this.eventUrl + id);
+}
+getAll(): Observable<Event[]>{
+return this.http.get<Event[]>(this.eventUrl);
+}
+}
---
+++ organiser/my-app/src/app/services/listToDo.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {ListToDo} from "../domens/listToDo";
+
+{
+providedIn: 'root'
+}
+export class ListToDoService {
+private listToDoUrl : string;
+
+constructor(private http: HttpClient) {
+this.listToDoUrl = "http://localhost:8182/api/user/3/lists/"; 
+}
+getById(id) : Observable<ListToDo>{
+return this.http.get<ListToDo>(this.listToDoUrl + id);
+}
+getAll(): Observable<ListToDo[]>{
+return this.http.get<ListToDo[]>(this.listToDoUrl);
+}
+}
