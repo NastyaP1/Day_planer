8921453f4a1bfcf5eca065146de4654e36be5aa2
NastyaP1 <anastasiapereslavtseva@gmail.com>
2020-06-08 11:25:05 +0300

    add yandex metrica and moved sidebar from global to each page

--- organiser/my-app/src/app/dayThings/app.dayThingsPageComponent.ts
+++ organiser/my-app/src/app/dayThings/app.dayThingsPageComponent.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, ViewChild, ElementRef, NgZone, Input, Output, EventEmitter, Injectable } from '@angular/core';
+import { Thing } from '../domens/thing';
+import { ThingService } from '../services/thing.service';
 
 @Component({
     templateUrl: './dayThingsPage.html',
@@ -6,11 +8,64 @@ import { Component, OnInit, ViewChild, ElementRef, NgZone, Input, Output, EventE
   })
 
   export class DayThingsPageComponent implements OnInit {
-    // currentUser: any;
+    things: Thing[];
+    listId: number;
+    thingId: number;
+    indexInArr: number;
+    visibility: boolean = true;
+    name;
+    description;
+    date;
+    daydate;
   
-    // constructor(private token: TokenStorageService,
-    //   ) { }
+  
+    constructor(
+    private thingService: ThingService,
+    )
+    {
+    }
+  
     ngOnInit() {
-    //   this.currentUser = this.token.getUser();
-    } 
+      console.log("hhhh" + sessionStorage.getItem("dayThings"));
+      this.things = JSON.parse(sessionStorage.getItem("dayThings"));
+      this.daydate =  this.things[0].date;
+    }
+  
+    close(){
+      this.visibility=!this.visibility;
+    }
+  
+    toggle(i){
+      this.visibility=!this.visibility;
+      this.name = this.things[i].name;
+      this.description = this.things[i].description;
+      this.date = this.things[i].date;
+      this.thingId = this.things[i].id;
+      this.listId = this.things[i].list.id;
+      this.indexInArr = i;
+    }
+    changeThingFalse(){
+      sessionStorage.setItem("changeThing", JSON.stringify(false));
+  }
+    delete(){
+      this.thingService.deleteById(this.thingId, this.listId).subscribe(data =>console.log(data));
+      this.things.splice(this.indexInArr, 1);
+      sessionStorage.setItem("things", JSON.stringify(this.things));
+      this.visibility=!this.visibility;
+    }
+    changeThing(){
+      sessionStorage.setItem("changeThing", JSON.stringify(true));
+      sessionStorage.setItem("thing", JSON.stringify(this.things[this.indexInArr]));
+      }
+      
+      changeState(thing: Thing){
+      if(thing.state == "true"){
+      thing.state = "false";
+      }
+      else{
+      thing.state = "true";
+      }
+      this.thingService.changeThing(thing.id,thing).subscribe(data =>console.log(data));
+      }
+    
   }
